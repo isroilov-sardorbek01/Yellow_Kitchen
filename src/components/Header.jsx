7c84f6c87940017c8350a8f91d72327ec0cba7fc
isroilov-sardor <notfalse01@gmail.com>
2025-03-25 21:10:14 +0500

    doing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import logoSite from "../images/Logo.svg";
+import cartImg from "../images/cart.svg";
+import burger from "../images/burger_menu.svg";
+import userImg from "../images/person.svg";
 
 function Header() {
-    return <div>
-        <div className="container">
-            
+    return (
+        <div className="p-[18px] border-b-[1px] border-b-[#D4D7E5] mb-[40px]">
+            <div className="container flex justify-between items-center">
+                <img
+                    className="select-none cursor-pointer"
+                    draggable={false}
+                    src={logoSite}
+                    width={184}
+                    height={24}
+                    alt=""
+                />
+                <div className="flex gap-5 justify-between items-center">
+                    <div className="flex gap-1 justify-between">
+                        <img src={userImg} alt="" width={24} height={24} />
+                        <h1 className="font-bold text-[#9093A6] text-[16px]">
+                            Log In
+                        </h1>
+                    </div>
+                    <div className="flex gap-1 justify-between">
+                        <img src={cartImg} width={24} height={24} alt="" />
+                        <h1 className="font-bold text-[#9093A6] text-[16px]">
+                            0
+                        </h1>
+                    </div>
+                    <div className="w-[48px] h-[48px] rounded-full bg-white shadow-md flex justify-center cursor-pointer hover:bg-yellow-300 transition-all hover:shadow-lg">
+                        <img src={burger} width={24} height={24} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>;
+    );
 }
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import homeImg from "../images/homeImg1.svg";
 
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div>
+            <div className="container flex items-center justify-between">
+                <div className="">
+                    <h1 className="text-[#3F4255] text-[56px] font-medium tracking-[3px] mb-[88px]">
+                        Your Food court at home
+                    </h1>
+                    <div className="flex items-center gap-[24px]">
+                        <div className="border-[2px] rounded-[4px] border-[#FFE145] px-[24px] py-[12px] text-center hover:shadow-md transition-all cursor-pointer hover:scale-125">
+                            <h1 className="font-bold text-[16px] text-[#3F4255]">
+                                Delivery
+                            </h1>
+                            <h1 className="font-normal text-[12px] text-[#3F4255]">
+                                Order in
+                            </h1>
+                        </div>
+                        <div className="border-[2px] rounded-[4px] border-[#FFE145] px-[24px] py-[12px] text-center hover:shadow-md transition-all cursor-pointer hover:scale-125">
+                            <h1 className="font-bold text-[16px] text-[#3F4255]">
+                                Takeout
+                            </h1>
+                            <h1 className="font-normal text-[12px] text-[#3F4255]">
+                                Grab and go
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <img
+                    src={homeImg}
+                    className="max-w-[803px] max-h-[706px]"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
