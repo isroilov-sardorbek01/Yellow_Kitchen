7ab16533a6eb54a46d9e14e1f8977243be5f0093
isroilov-sardor <notfalse01@gmail.com>
2025-03-26 10:42:32 +0500

    done burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import logoSite from "../images/Logo.svg";
 import cartImg from "../images/cart.svg";
 import burger from "../images/burger_menu.svg";
 import userImg from "../images/person.svg";
+import closeImg from "../images/closeMenuImg.svg";
+import callImg from "../images/call.svg";
 
 function Header() {
+    const [modal, setModal] = useState(false);
+    const [animate, setAnimate] = useState(false);
+    function handleOpenMenu() {
+        console.log("hello");
+        setModal(true);
+    }
+    function handleCloseMenu() {
+        setAnimate(true);
+        setTimeout(() => {
+            setModal(false);
+            setAnimate(false);
+        }, 400);
+    }
     return (
-        <div className="p-[18px] border-b-[1px] border-b-[#D4D7E5] mb-[40px]">
-            <div className="container flex justify-between items-center">
+        <div className="p-[18px] border-b-[1px] border-b-[#D4D7E5] mb-[40px] relative">
+            <div className="container flex items-center justify-between">
                 <img
-                    className="select-none cursor-pointer"
+                    className="cursor-pointer select-none"
                     draggable={false}
                     src={logoSite}
                     width={184}
                     height={24}
                     alt=""
                 />
-                <div className="flex gap-5 justify-between items-center">
-                    <div className="flex gap-1 justify-between">
+                <div className="flex items-center justify-between gap-5">
+                    <div className="flex justify-between gap-1">
                         <img src={userImg} alt="" width={24} height={24} />
                         <h1 className="font-bold text-[#9093A6] text-[16px]">
                             Log In
                         </h1>
                     </div>
-                    <div className="flex gap-1 justify-between">
+                    <div className="flex justify-between gap-1">
                         <img src={cartImg} width={24} height={24} alt="" />
                         <h1 className="font-bold text-[#9093A6] text-[16px]">
                             0
                         </h1>
                     </div>
-                    <div className="w-[48px] h-[48px] rounded-full bg-white shadow-md flex justify-center cursor-pointer hover:bg-yellow-300 transition-all hover:shadow-lg">
+                    <div
+                        onClick={handleOpenMenu}
+                        className="w-[48px] h-[48px] rounded-full bg-white shadow-md flex justify-center cursor-pointer hover:bg-yellow-300 transition-all hover:shadow-lg"
+                    >
                         <img src={burger} width={24} height={24} alt="" />
                     </div>
                 </div>
             </div>
+            {modal === true ? (
+                <>
+                    <div className="fixed inset-0 bg-[#797979] opacity-70 z-40"></div>
+                    <div
+                        className={`fixed right-0 top-0 z-50 w-[380px] pt-[20px] bg-white h-[100%] ${
+                            animate === true
+                                ? "animate-slideOutRight"
+                                : "animate-slideInRight"
+                        }`}
+                    >
+                        <div className="p-5">
+                            <div className="flex items-center justify-between mb-5">
+                                <img
+                                    src={logoSite}
+                                    className=""
+                                    width={150}
+                                    alt=""
+                                />
+                                <div
+                                    onClick={handleCloseMenu}
+                                    className="w-[48px] h-[48px] rounded-full bg-white shadow-md flex justify-center cursor-pointer hover:bg-yellow-300 transition-all hover:shadow-lg"
+                                >
+                                    <img
+                                        width={24}
+                                        height={24}
+                                        src={closeImg}
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="">
+                                <div className="flex items-center gap-4 mb-2">
+                                    <img
+                                        src={callImg}
+                                        width={24}
+                                        height={24}
+                                        alt=""
+                                    />
+                                    <h1 className="text-[#3F4255] text-[16px] font-bold">
+                                        0800 111 126
+                                    </h1>
+                                </div>
+                                <li className="text-[#9093A6] text-[12px] mb-[24px]">
+                                    8:00 - 22:00
+                                </li>
+                            </div>
+                            <hr />
+                            <div className="mt-[22px] mb-[22px]">
+                                <h1 className="text-[16px] text-[#9093A6] font-bold cursor-pointer">
+                                    Delivery info
+                                </h1>
+                            </div>
+                            <hr />
+                            <div className="mt-[22px] mb-[22px]">
+                                <h1 className="text-[16px] text-[#9093A6] font-bold cursor-pointer">
+                                    About us
+                                </h1>
+                            </div>
+                            <hr />
+                            <div className="mt-[22px] mb-[22px]">
+                                <h1 className="text-[16px] text-[#9093A6] font-bold cursor-pointer">
+                                    Restaraunts
+                                </h1>
+                            </div>
+                            <hr />
+                            <div className="mt-[22px] mb-[22px]">
+                                <h1 className="text-[16px] text-[#9093A6] font-bold cursor-pointer">
+                                    Specialities
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <></>
+            )}
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            keyframes: {
+                slideInRight: {
+                    "0%": { transform: "translateX(100%)", opacity: "0" },
+                    "100%": { transform: "translateX(0)", opacity: "1" },
+                },
+                slideOutRight: {
+                    "0%": { transform: "translateX(0)", opacity: "1" },
+                    "100%": { transform: "translateX(100%)", opacity: "0" },
+                },
+            },
+            animation: {
+                slideInRight: "slideInRight 0.5s ease-out ",
+                slideOutRight: "slideOutRight 0.5s ease-out ",
+            },
+        },
+    },
+    darkMode: "class",
+    plugins: [],
+};
